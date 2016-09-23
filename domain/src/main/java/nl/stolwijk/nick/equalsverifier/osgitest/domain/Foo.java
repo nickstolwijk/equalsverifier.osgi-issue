b6ad2ec21272e858c45c733b546b8b564d111ae5
Nick Stolwijk <nick.stolwijk@rigd-loxia.nl>
2016-09-23 16:28:27 +0200

    Also run equalsverifier with a succesfull test.

--- domain/src/main/java/nl/stolwijk/nick/equalsverifier/osgitest/domain/Foo.java
+++ domain/src/main/java/nl/stolwijk/nick/equalsverifier/osgitest/domain/Foo.java
@@ -1,5 +1,6 @@
 package nl.stolwijk.nick.equalsverifier.osgitest.domain;
 
+import java.util.Objects;
 import org.eclipse.jdt.annotation.NonNull;
 
 public final class Foo {
@@ -12,6 +13,19 @@ public final class Foo {
 
     @Override
     public boolean equals(Object other) {
-        return bar.equals(((Foo) other).bar);
+        if (other == null) { return false; }
+        if (other == this) { return true; }
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        Foo rhs = (Foo) other;
+        return Objects.equals(bar, rhs.bar);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash += (this.bar != null ? this.bar.hashCode() : 0);
+        return hash;
     }
 }
